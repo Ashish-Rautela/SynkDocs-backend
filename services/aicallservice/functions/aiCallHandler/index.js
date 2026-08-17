3b26e738aa6934bc72d9dd626d9c1ebca0bbf9f5
Ashish-Rautela <chessashishrautela@gmail.com>
2026-08-17 09:44:24 +0530

    removed unwanted lambdas expense tracker and aicalls

--- services/aicallservice/functions/aiCallHandler/index.js
+++
@@ -1,9 +0,0 @@
-const AICallService = require('./service');
-const AICallHandler = require('./handler');
-
-const service = new AICallService();
-const handlerInstance = new AICallHandler(service);
-
-exports.handler = async (event) => {
-  return await handlerInstance.handle(event);
-};
